1d0e6f514f5c3d27860e2830a8ba668a83dfeded
RobotGrrl <erin@robotgrrl.com>
2022-06-06 23:16:10 -0400

    LCD and touch screen hit testing

---
+++ experimental/testing21_L4/Debug/sources.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/BSP/Components/cs42l51 \
+Drivers/BSP/Components/ft6x06 \
+Drivers/BSP/Components/mfxstm32l152 \
+Drivers/BSP/Components/ov9655 \
+Drivers/BSP/Components/st7789h2 \
+Drivers/BSP/STM32L496G-Discovery \
+Drivers/STM32L4xx_HAL_Driver/Src \
+Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Src \
+Middlewares/ST/STM32_USB_Device_Library/Core/Src \
+USB_DEVICE/App \
+USB_DEVICE/Target \
+
